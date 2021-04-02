2d61fad0b408c75b0c03d39770da9b02391e2087
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-04-03 01:39:58 +0200

    loading simple tilemap

---
+++ core/assets/bombermanTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="tilemap" tilewidth="16" tileheight="16" tilecount="48" columns="8">
+ <image source="tilemap.png" width="128" height="96"/>
+</tileset>
